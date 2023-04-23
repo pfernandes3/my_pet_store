ee75dc558c54b4bf4babd24606bbd84f10585e61
Pedro Fernandes <pedrofernandes_3@hotmail.com>
2023-04-23 16:22:47 -0300

    Insert Products

--- lib/views/product_form_screen.dart
+++ lib/views/product_form_screen.dart
@@ -1,5 +1,7 @@
 import 'package:my_pet_store/imports.dart';
+import 'package:my_pet_store/providers/products_provider.dart';
 import 'package:my_pet_store/widgets/custom_text_form_field.dart';
+import 'package:provider/provider.dart';
 
 import '../models/pet_products.dart';
 
@@ -39,6 +41,64 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
     }
   }
 
+  void _updateImageUrl() {
+    if (isValidateImageUrl(_imgController.text)) {
+      setState(() {});
+    }
+  }
+
+  Future<void> _saveForm() async {
+    bool isValid = _formKey.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+
+    _formKey.currentState?.save();
+
+    final newProduct = Product(
+      id: _formData['id'],
+      title: _formData['title'],
+      description: _formData['description'],
+      price: _formData['price'],
+      imageUrl: _formData['imageUrl'],
+    );
+
+    setState(() {
+      isLoading = true;
+    });
+
+    final products = Provider.of<ProductsProvider>(context, listen: false);
+
+    try {
+      if (_formData['id'] == null) {
+        await products.insertProducts(newProduct);
+        await products.loadProducts();
+      } else {
+        await products.updateProducts(newProduct);
+        await products.loadProducts();
+      }
+
+      Navigator.of(context).pop();
+    } catch (e) {
+      await showDialog<void>(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: const Text('Ocorreu um erro!'),
+                content: const Text(
+                    'Ocorreu um erro inesperado ao salvar o produto!'),
+                actions: [
+                  TextButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text('OK')),
+                ],
+              ));
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
   bool isValidateImageUrl(String url) {
     bool startWithHttp = url.toLowerCase().startsWith('http://');
     bool startWithHttps = url.toLowerCase().startsWith('https://');
@@ -60,7 +120,7 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
         title: const Text('Cadastro de Produtos'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _saveForm,
             icon: const Icon(Icons.save),
           )
         ],
@@ -90,6 +150,9 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                 const SizedBox(
                   height: 20,
                 ),
+                const SizedBox(
+                  height: 20,
+                ),
                 Custom_Form_Field(
                   label: const Text('Preço'),
                   icon: const Icon(Icons.monetization_on),
@@ -109,23 +172,8 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                   height: 20,
                 ),
                 Custom_Form_Field(
-                  label: const Text('Preço'),
-                  icon: const Icon(Icons.monetization_on),
-                  initialValue: _formData['price'].toString(),
-                  validator: (value) {
-                    if (value!.trim().isEmpty ||
-                        double.tryParse(value) == null ||
-                        double.tryParse(value)! <= 0) {
-                      return 'Informe um valor válido';
-                    }
-                    return null;
-                  },
-                  onsaved: (value) => _formData['price'] = double.parse(value),
-                  textinputaction: TextInputAction.next,
-                ),
-                Custom_Form_Field(
                   label: const Text('Descrição'),
-                  icon: const Icon(Icons.monetization_on),
+                  icon: const Icon(Icons.description),
                   initialValue: _formData['description'],
                   validator: (value) {
                     if (value!.trim().isEmpty) {
@@ -140,7 +188,8 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Expanded(child: TextFormField(
+                    Expanded(
+                        child: TextFormField(
                       validator: (value) {
                         if (value!.trim().isEmpty ||
                             !isValidateImageUrl(value)) {
@@ -148,14 +197,37 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                         }
                         return null;
                       },
-
-                      decoration: const InputDecoration(
-                        labelText: 'Url da Imagem'
-                      ),
+                    
+                      decoration:
+                          const InputDecoration(labelText: 'Url da Imagem',prefixIcon: Icon(Icons.image)),
                       keyboardType: TextInputType.url,
                       textInputAction: TextInputAction.done,
-                      
-                    ))
+                      controller: _imgController,
+                      focusNode: _imgFocus,
+                      onFieldSubmitted: (_) {
+                        _saveForm();
+                      },
+                      onSaved: (value) =>
+                          _formData['imageUrl'] = value as String,
+                    )),
+                    Container(
+                        width: 200,
+                        height: 200,
+                        margin: const EdgeInsets.only(top: 25, left: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey, width: 1),
+                        ),
+                        alignment: Alignment.center,
+                        child: _imgController.text.isEmpty
+                            ? const Text(
+                                'Informe a URL',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontSize: 25),
+                              )
+                            : Image.network(
+                                _imgController.text,
+                                fit: BoxFit.cover,
+                              ))
                   ],
                 )
               ],
